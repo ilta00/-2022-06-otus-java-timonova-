49ae9b2df4725cec1a0829ec07afb532ad6f1bbe
timonova <timonova@axsoft.ru>
2022-11-09 10:50:52 +0300

    deque

--- hw2-collections/src/main/java/ru/otus/timonova/CustomerReverseOrder.java
+++ hw2-collections/src/main/java/ru/otus/timonova/CustomerReverseOrder.java
@@ -1,10 +1,11 @@
 package ru.otus.timonova;
 
 
+import java.util.Deque;
 import java.util.LinkedList;
 
 public class CustomerReverseOrder {
-    private LinkedList<Customer> customers = new LinkedList<>();
+    private Deque<Customer> customers = new LinkedList<>();
 
     //todo: 2. надо реализовать методы этого класса
     //надо подобрать подходящую структуру данных, тогда решение будет в "две строчки"
