db058a9122e6183583c391e0e4262ff12cdb896d
timonova <timonova@axsoft.ru>
2022-11-12 13:30:54 +0300

    hw03

---
+++ hw03-annotations/src/main/java/ru/otus/timonova/annotations/After.java
@@ -0,0 +1,11 @@
+package main.java.ru.otus.timonova.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(java.lang.annotation.ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface After {
+}
